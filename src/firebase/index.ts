a3c034b8c52520958fb1c4ee8d466f76e7ff5680
Salman Khaskheli <salmankhaskheli885@gmail.com>
2025-10-14 20:19:14 +0000

    AIzaSyDVdiPQ_CWI6KbdXjTd1-VxzsNSdMgXQFA

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,8 +4,8 @@ import { type FirebaseApp } from 'firebase/app';
 // This is the one and only source of truth for the Firebase config.
 export const firebaseConfig = {
     "projectId": "fynixpro222",
-    "appId": "1:652865910892:web:aa38865298818c399b245a",
-    "apiKey": "AIzaSyCHi4113Y_DBQh_8N7uXxy3xICe9kdtYDE",
+    "appId": "1:652865910892:web:aa38865298818c3_99b245a",
+    "apiKey": "AIzaSyDVdiPQ_CWI6KbdXjTd1-VxzsNSdMgXQFA",
     "authDomain": "fynixpro222.firebaseapp.com",
     "storageBucket": "fynixpro222.appspot.com",
     "messagingSenderId": "652865910892"
