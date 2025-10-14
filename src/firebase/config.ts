a34ddae0892c772e63270da11e362b996e417bb2
Salman Khaskheli <salmankhaskheli885@gmail.com>
2025-10-14 16:18:09 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,10 +5,10 @@
 // this file in your source control. See https://firebase.google.com/docs/web/start
 // for more information.
 export const firebaseConfig = {
-  "apiKey": process.env.NEXT_PUBLIC_API_KEY,
-  "authDomain": process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  "projectId": process.env.NEXT_PUBLIC_PROJECT_ID,
-  "storageBucket": process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  "messagingSenderId": process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  "appId": process.env.NEXT_PUBLIC_APP_ID
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
