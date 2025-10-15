fa183d3a067f8dd02be0257b3c5ec346b898f31f
Salman Khaskheli <salmankhaskheli885@gmail.com>
2025-10-15 19:46:19 +0000

    auth domain do

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,11 +3,11 @@ import { type FirebaseApp } from 'firebase/app';
 
 // This is the one and only source of truth for the Firebase config.
 export const firebaseConfig = {
-    "projectId": "fynixpro222",
+    "projectId": "treazino-a1a7a",
     "appId": "1:652865910892:web:aa38865298818c3_99b245a",
     "apiKey": "AIzaSyDVdiPQ_CWI6KbdXjTd1-VxzsNSdMgXQFA",
-    "authDomain": "fynixpro222.firebaseapp.com",
-    "storageBucket": "fynixpro222.appspot.com",
+    "authDomain": "treazino-a1a7a.firebaseapp.com",
+    "storageBucket": "treazino-a1a7a.appspot.com",
     "messagingSenderId": "652865910892"
 };
 
