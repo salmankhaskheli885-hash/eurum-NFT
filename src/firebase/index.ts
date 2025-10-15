44eeb07d6081fde40d2744e535d5a040eb98e971
Salman Khaskheli <salmankhaskheli885@gmail.com>
2025-10-15 20:15:24 +0000

    File changes

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,11 +3,11 @@ import { type FirebaseApp } from 'firebase/app';
 
 // This is the one and only source of truth for the Firebase config.
 export const firebaseConfig = {
-    "projectId": "fynixpro-216f9",
+    "projectId": "fynix-new-auth-fix",
     "appId": "1:807029821919:web:9e4d3a35a64c4891b799a7",
     "apiKey": "AIzaSyD0ulSSjV6rQvsjWgRu8FPEcFzMWVafxPM",
-    "authDomain": "fynixpro-216f9.firebaseapp.com",
-    "storageBucket": "fynixpro-216f9.appspot.com",
+    "authDomain": "fynix-new-auth-fix.firebaseapp.com",
+    "storageBucket": "fynix-new-auth-fix.appspot.com",
     "messagingSenderId": "807029821919"
 };
 
