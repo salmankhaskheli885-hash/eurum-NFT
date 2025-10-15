8b509f79a76738bf8620e20e738fb74f5b27dd1c
Salman Khaskheli <salmankhaskheli885@gmail.com>
2025-10-15 20:32:07 +0000

    AIzaSyAO3uFKtPRIheJghLWDOBBdmj0eZe0eYDQ

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,10 +1,11 @@
+
 import { initializeApp, getApp, getApps, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
 
 // Note: Replace this with your actual Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyD0ulSSjV6rQvsjWgRu8FPEcFzMWVafxPM",
+  apiKey: "AIzaSyAO3uFKtPRIheJghLWDOBBdmj0eZe0eYDQ",
   authDomain: "fynix-new-auth-fix.firebaseapp.com",
   projectId: "fynix-new-auth-fix",
   storageBucket: "fynix-new-auth-fix.appspot.com",
