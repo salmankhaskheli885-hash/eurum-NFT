bb4360f4bb0ece51690a01cecd1217f00badf197
Salman Khaskheli <salmankhaskheli885@gmail.com>
2025-10-15 19:52:33 +0000

    domain change kro

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,11 +3,11 @@ import { type FirebaseApp } from 'firebase/app';
 
 // This is the one and only source of truth for the Firebase config.
 export const firebaseConfig = {
-    "projectId": "treazino-a1a7a",
+    "projectId": "earnify-7f26c",
     "appId": "1:652865910892:web:aa38865298818c3_99b245a",
     "apiKey": "AIzaSyDVdiPQ_CWI6KbdXjTd1-VxzsNSdMgXQFA",
-    "authDomain": "treazino-a1a7a.firebaseapp.com",
-    "storageBucket": "treazino-a1a7a.appspot.com",
+    "authDomain": "earnify-7f26c.firebaseapp.com",
+    "storageBucket": "earnify-7f26c.appspot.com",
     "messagingSenderId": "652865910892"
 };
 
