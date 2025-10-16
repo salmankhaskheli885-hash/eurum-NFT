91b0a8582b85d3259697c96505e2b80124f4bc55
Salman Khaskheli <salmankhaskheli885@gmail.com>
2025-10-16 11:31:15 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // The app directory is enabled by default in Next.js 13.4+
-  // We are keeping this explicit for clarity.
-  experimental: {
-    appDir: true,
-  },
   // Add any other Next.js configurations here if needed in the future.
 };
 
