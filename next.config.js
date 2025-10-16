97d649fe0c78072bb23ef39d3855f0e53cbfe868
Salman Khaskheli <salmankhaskheli885@gmail.com>
2025-10-16 10:12:40 +0000

    Publish your app
    Follow these steps to set up your project and publish

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -7,6 +8,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
