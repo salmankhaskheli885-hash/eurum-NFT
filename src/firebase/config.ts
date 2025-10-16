112291d95146e65afecc58519d3e9cf8d8cb0002
Salman Khaskheli <salmankhaskheli885@gmail.com>
2025-10-16 07:47:37 +0000

    AIzaSyDVdiPQ_CWI6KbdXjTd1-VxzsNSdMgXQFA

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,11 +2,10 @@
 // NOTE: This is a public-facing configuration object.
 // No sensitive data should be stored here.
 export const firebaseConfig = {
-  apiKey: "AIzaSyBe_e24Da_xTj_G09e2t_v3-R3hJkL0A",
+  apiKey: "AIzaSyDVdiPQ_CWI6KbdXjTd1-VxzsNSdMgXQFA",
   authDomain: "fynixpro222.firebaseapp.com",
   projectId: "fynixpro222",
   storageBucket: "fynixpro222.appspot.com",
   messagingSenderId: "367098418044",
   appId: "1:367098418044:web:0f4e3c938c62c2f7b1b3a1"
 };
-
