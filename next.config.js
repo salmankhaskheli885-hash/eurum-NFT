99879b25f6e2841bc25310b29adfc1e9da0394af
Salman Khaskheli <salmankhaskheli885@gmail.com>
2025-10-16 10:15:24 +0000

    Publish your app
    Follow these steps to set up your project and publish

--- next.config.js
+++ next.config.js
@@ -9,26 +9,6 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
