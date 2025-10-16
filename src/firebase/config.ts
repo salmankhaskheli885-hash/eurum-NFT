41ccf56ccd0aa6cd8bd02e2dee616cec57f5b949
Salman Khaskheli <salmankhaskheli885@gmail.com>
2025-10-16 07:46:15 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,10 +2,11 @@
 // NOTE: This is a public-facing configuration object.
 // No sensitive data should be stored here.
 export const firebaseConfig = {
-  apiKey: "AIzaSyAO3uFKtPRIheJghLWDOBBdmj0eZe0eYDQ",
-  authDomain: "fynix-new-auth-fix.firebaseapp.com",
-  projectId: "fynix-new-auth-fix",
-  storageBucket: "fynix-new-auth-fix.appspot.com",
+  apiKey: "AIzaSyBe_e24Da_xTj_G09e2t_v3-R3hJkL0A",
+  authDomain: "fynixpro222.firebaseapp.com",
+  projectId: "fynixpro222",
+  storageBucket: "fynixpro222.appspot.com",
   messagingSenderId: "367098418044",
   appId: "1:367098418044:web:0f4e3c938c62c2f7b1b3a1"
 };
+
