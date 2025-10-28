6ab0186a827ad0d2704c3eed068774ab8fce12d1
Salman Khaskheli <salmankhaskheli885@gmail.com>
2025-10-28 13:23:23 +0000

    nhi ho raha yar fix krwao

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,7 +6,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDVPye27f-R_v6K9yj77butYlGkhVLWRyQ",
   authDomain: "fynix-pro.firebaseapp.com",
   projectId: "fynix-pro",
-  storageBucket: "fynix-pro.firebasestorage.app",
+  storageBucket: "fynix-pro.appspot.com",
   messagingSenderId: "864439401863",
   appId: "1:864439401863:web:5ed4d0dc0ca9fbfdd5a722",
   measurementId: "G-D13PLSZ6C4"
